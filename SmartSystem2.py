35f318ff7e91c63069f03a4f2e2ce19ca2b43a20
jakeroggenbuck <jake>
2019-10-30 16:42:22 -0700

    add f for formatting and add commands

--- SmartSystem2.py
+++ SmartSystem2.py
@@ -1,8 +1,8 @@
 # import the libraries
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 # setmode something
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 
 # lists the GPIO pins in list for easy acsses
 relayList = [26, 19, 13, 6, 3, 22, 27, 17]
@@ -12,7 +12,7 @@ relayState = []
 def RelaySetup(pin, state, num):
     #GPIO.setup(pin, GPIO.OUT)
     relayState.append(state)
-    print("Relay on pin " + str(pin) + " number " + str(num) + " is " + str(state))
+    print(f"Relay on pin {pin} number {num} is {state}")
 
 def RelayLoop(setState):
     for x in range(len(relayList)):
@@ -22,19 +22,19 @@ def setMode():
     print("Set Mode")
     setWhat = int(input("What relay would you like to set? 0-8 "))
     if relayState[setWhat] == True:
-        GPIO.output(relayList[setWhat], GPIO.LOW)
-        print("Relay " + str(setWhat) + " on pin " + str(relayList[setWhat]) + " has been set LOW")
+        #GPIO.output(relayList[setWhat], GPIO.LOW)
+        print(f"Relay {setWhat} on pin {relayList[setWhat]} has been set LOW")
         relayState[setWhat] = False 
     elif relayState[setWhat] == False:
-        GPIO.output(relayList[setWhat], GPIO.HIGH)
-        print("Relay " + str(setWhat) + " on pin " + str(relayList[setWhat]) + " has been set HIGH")
+        #GPIO.output(relayList[setWhat], GPIO.HIGH)
+        print(f"Relay {setWhat} on pin {relayList[setWhat]} has been set HIGH")
         relayState[setWhat] = True
     else:
         print("Not Valid")
 
 def checkMode():
     for x in range(len(relayList)):
-        print("Relay " + str(x) + " on pin " + str(relayList[x]) + " is " + str(relayState[x]))
+        print(f"Relay {x} on pin {relayList[x]} is {relayState[x]}")
 
 def stopMode():
     sure = input("Are you sure you want to close? Y/n ")
@@ -54,8 +54,8 @@ while(True):
     if commandInput[0:3] == "set":
         setMode()
 
-    elif commandInput[0:4] == "stop":
+    elif commandInput[0:4] == "stop" or commandInput[0:1] == "q":
         stopMode()
 
-    elif commandInput[0:5] == "check":
+    elif commandInput[0:5] == "check" or commandInput[0:2] == "ls":
         checkMode()
