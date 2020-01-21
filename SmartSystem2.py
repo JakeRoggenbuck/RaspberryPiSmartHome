ca6e27b8b3dba0642336c6410992365f130acbfe
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-01-20 22:04:04 -0800

    Revise small thing and improve PEP8 formating

--- SmartSystem2.py
+++ SmartSystem2.py
@@ -14,13 +14,15 @@ def RelaySetup(pin, state, num):
     relayState.append(state)
     print(f"Relay on pin {pin} number {num} is {state}")
 
+
 def RelayLoop(setState):
     for x in range(len(relayList)):
         RelaySetup(relayList[x], setState, x)
 
+
 def setMode():
     print("Set Mode")
-    setWhat = int(input("What relay would you like to set? 0-8 "))
+    setWhat = int(input("What relay would you like to set? 0-7 "))
     if relayState[setWhat] == True:
         #GPIO.output(relayList[setWhat], GPIO.LOW)
         print(f"Relay {setWhat} on pin {relayList[setWhat]} has been set LOW")
@@ -32,30 +34,34 @@ def setMode():
     else:
         print("Not Valid")
 
+
 def checkMode():
     for x in range(len(relayList)):
         print(f"Relay {x} on pin {relayList[x]} is {relayState[x]}")
 
+
 def stopMode():
     sure = input("Are you sure you want to close? Y/n ")
     if sure == "Y" or sure == "y":
-        print("Relays Off")
-        print("Closed")
+        RelayLoop(False)
+        print("Relays Off\nClosed")
         exit()
     else:
         print("Returning")
+
 
 RelayLoop(True)
 print("Relays Powered")
 
 while(True):
     commandInput = input("$ ")
-    
-    if commandInput[0:3] == "set":
-        setMode()
 
-    elif commandInput[0:4] == "stop" or commandInput[0:1] == "q":
+    if commandInput[0:1] == "q":
         stopMode()
 
-    elif commandInput[0:5] == "check" or commandInput[0:2] == "ls":
+    elif commandInput[0:2] == "ls":
         checkMode()
+
+    elif commandInput[0:3] == "set":
+        setMode()
+
