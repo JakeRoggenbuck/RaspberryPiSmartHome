bb091c296c491b5339a51a2337e3ac3950ee55b5
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-03-14 16:21:29 -0700

    Change low to 0 and high to 1

--- SmartSystem.py
+++ SmartSystem.py
@@ -1,38 +1,30 @@
 # import the libraries
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 # setmode something
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 
 class Relay:
-    def __init__(self, pin, name, status='LOW'):
+    def __init__(self, pin, name, status=0):
         self.pin = pin
         self.name = name
         self.status = status
         try:
             GPIO.setup(pin, GPIO.OUT)
-            print('OK')
         except:
-            print('Error occurred when trying to init relay on pin ' + str(pin))
+            print(f'Error occurred when trying to init relay on pin ' + str(pin))
             exit()
 
-    def toggle(obj):
-        if obj.status == 'HIGH':
-            GPIO.output(obj.pin, GPIO.LOW)
-            obj.status = 'LOW'
+    def toggle(self):
+        if self.status == 1:
+            GPIO.output(self.pin, 0)
+            self.status = 0
         else:
-            GPIO.output(obj.pin, GPIO.HIGH)
-            obj.status = 'HIGH'
+            GPIO.output(self.pin, 1)
+            self.status = 1
 
-    def set(obj, fstat):
-        GPIO.output(obj.pin, eval(GPIO.fstat)
-        obj.status = fstat
-
+    def set(self, fstat):
+        GPIO.output(self.pin, fstat)
+        self.status = fstat
 
 light = Relay(26, "Desk Light")
-print(light.name, light.pin, light.status)
-
-light.set('HIGH')
-print(light.name, light.pin, light.status)
-
-light.toggle()
 print(light.name, light.pin, light.status)
