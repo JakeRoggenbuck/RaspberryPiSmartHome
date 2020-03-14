347a37167433cb62badd470c177aab39c33a8a45
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-03-14 13:44:21 -0700

    Remove f string because of old debian

--- SmartSystem.py
+++ SmartSystem.py
@@ -7,7 +7,7 @@ class Relay:
             GPIO.setup(pin, GPIO.OUT)
             print('OK')
         except:
-            print(f'Error occurred when trying to init relay on pin {pin}')
+            print('Error occurred when trying to init relay on pin ' + pin)
             exit()
 
     def toggle(obj):
